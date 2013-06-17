75ade3204027dd8f730521c472577c138ede2dde
Louisa Freymond <louisa.freymond@heig-vd.ch>
2013-06-17 16:04:49 +0200

    html partner v2

---
+++ includes/Smarty/templates_c/60c84743750cf924f7b92afc82d420f072caafcb.file.contact.tpl.php
@@ -0,0 +1,26 @@
+<?php /* Smarty version Smarty-3.1.13, created on 2013-06-17 14:02:05
+         compiled from "includes\templates\contact.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:1879551bf16dd69a8b2-99597344%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '60c84743750cf924f7b92afc82d420f072caafcb' => 
+    array (
+      0 => 'includes\\templates\\contact.tpl',
+      1 => 1371453851,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '1879551bf16dd69a8b2-99597344',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.13',
+  'unifunc' => 'content_51bf16dd8d3515_56360218',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_51bf16dd8d3515_56360218')) {function content_51bf16dd8d3515_56360218($_smarty_tpl) {?>    </nav>
+</header>
+<section id="content">
+	<h1>Welcome at contact page</h1>
+</section><?php }} ?>
