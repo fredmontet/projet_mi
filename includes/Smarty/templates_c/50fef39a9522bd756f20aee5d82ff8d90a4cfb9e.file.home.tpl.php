7144d4ecccb7c12914be2f991d0b1fa317df2f6b
Louisa Freymond <louisa.freymond@heig-vd.ch>
2013-06-17 16:00:17 +0200

    shit

---
+++ includes/Smarty/templates_c/50fef39a9522bd756f20aee5d82ff8d90a4cfb9e.file.home.tpl.php
@@ -0,0 +1,29 @@
+<?php /* Smarty version Smarty-3.1.13, created on 2013-06-17 07:24:43
+         compiled from "includes\templates\home.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:2655351beb9bb0cd265-88605540%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '50fef39a9522bd756f20aee5d82ff8d90a4cfb9e' => 
+    array (
+      0 => 'includes\\templates\\home.tpl',
+      1 => 1371453851,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '2655351beb9bb0cd265-88605540',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.13',
+  'unifunc' => 'content_51beb9bb126ae7_54624665',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_51beb9bb126ae7_54624665')) {function content_51beb9bb126ae7_54624665($_smarty_tpl) {?>		<ul id="subnav">
+			
+		</ul>
+    </nav>
+</header>
+<section id="content">
+	<h1>Welcome at home page</h1>
+</section><?php }} ?>
